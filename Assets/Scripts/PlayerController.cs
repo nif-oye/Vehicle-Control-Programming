671e6e593f09bca731ca0b4a50797146c2383811
Oluwanifemi <oluwanifemi.dev@gmail.com>
2024-08-07 20:48:16 +0100

    commit

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 10f;
+    public float turnSpeed;
+    public float horizontalInput;
     void Start()
     {
         
@@ -12,6 +14,8 @@ public class PlayerController : MonoBehaviour
     
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(speed * Time.deltaTime * Vector3.forward);
+        transform.Translate(turnSpeed * Time.deltaTime * Vector3.right * horizontalInput);
     }
 }
