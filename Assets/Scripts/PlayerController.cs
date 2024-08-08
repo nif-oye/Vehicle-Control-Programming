9d713a3f2ddd2f8c65e020f53ec3475d0db1fe3b
Oluwanifemi <oluwanifemi.dev@gmail.com>
2024-08-08 15:47:22 +0100

    Completed car control

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 10f;
-    public float turnSpeed;
-    public float horizontalInput;
+    private float speed = 40f;
+    private float turnSpeed = 50f;
+    private float horizontalInput;
+    private float forwardInput;
     void Start()
     {
         
@@ -15,7 +16,8 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
-        transform.Translate(speed * Time.deltaTime * Vector3.forward);
-        transform.Translate(turnSpeed * Time.deltaTime * Vector3.right * horizontalInput);
+        forwardInput = Input.GetAxis("Vertical");
+        transform.Translate(speed * Time.deltaTime * Vector3.forward * forwardInput);
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
